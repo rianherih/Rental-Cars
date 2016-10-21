d4ecb05d65395d2ee802eb3d11149b52a2064d0c
Rian Heri H <rianherih@gmail.com>
2016-10-22 06:29:27 +0700

    no message

---
+++ RianHerih/app/src/main/java/test/application/rianherih/FirstFragment.java
@@ -0,0 +1,21 @@
+package test.application.rianherih;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Rian Heri H on 10/21/2016.
+ */
+public class FirstFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_first,
+                container, false);
+        return rootView;
+    }
+}
---
+++ RianHerih/app/src/main/java/test/application/rianherih/ThirdFragment.java
@@ -0,0 +1,20 @@
+package test.application.rianherih;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Rian Heri H on 10/21/2016.
+ */
+public class ThirdFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_third,
+                container, false);
+        return rootView;
+    }
+}
